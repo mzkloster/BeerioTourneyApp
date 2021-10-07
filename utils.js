0f50c129943917e8574931bb7906f6ec0ceee691
Fredrik Kloster <fredrik.kloster@vipps.no>
2021-10-07 19:07:27 +0200

    forkortet enda mer

--- utils.js
+++ utils.js
@@ -16,43 +16,8 @@ function isMobileView(){
     monthValue = createdDate.getMonth();
     year = createdDate.getFullYear();
 
-    switch (monthValue) {        
-        case 0:
-            month = "januar";
-            break;
-        case 1:
-            month = "februar";
-            break;
-        case 2:
-            month = "mars";
-            break;
-        case 3:
-            month = "april";
-            break;
-        case 4:
-            month = "mai";
-            break;
-        case 5:
-            month = "juni";
-            break;
-        case 6:
-            month = "juli";
-            break;
-        case 7:
-            month = "august";
-            break;
-        case 8:
-            month = "september";
-            break;
-        case 9:
-            month = "oktober";
-            break;
-        case 10:
-            month = "november";
-            break;
-        case 11:
-            month = "desember";
-    }
+    const monthList = ["januar","februar","mars","april","mai","juni","juli","august","september","oktober","november","desember"];
+    month = monthList[monthValue];
 
     if(isMobileView()){
         month = month.slice(0,3);
