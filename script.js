8e5778506d71f13e3fa2186648515528cf413e9e
mzkloster <martin.kloster91@gmail.com>
2023-12-01 20:44:18 +0100

    refactoring round 4

--- script.js
+++ script.js
@@ -947,6 +947,32 @@ function getPlayerPlacementInMatch(gameId, playerName, matchId){
     }    
 }
 
+
+/**
+ * Returns createdDate for game in string format "5. januar 2021"
+ * @param {string} gameId 
+ * @returns string
+ */
+function getCreatedDateToString(gameId){
+    let day, monthValue, month, year, dateString;
+    const parsedGameObj = getParsedGameObj(gameId);
+    const createdDate = new Date(parsedGameObj['createdDate']);
+    day = createdDate.getDate();
+    monthValue = createdDate.getMonth();
+    year = createdDate.getFullYear();
+
+    const monthList = ["januar","februar","mars","april","mai","juni","juli","august","september","oktober","november","desember"];
+    month = monthList[monthValue];
+
+    if(isMobileView()){
+        month = month.slice(0,3);
+    }
+
+    dateString = day + '. ' + month + ' ' + year;
+    return dateString;
+}
+
+
 /**
  * Returns a sorted list of playerNames base on points
  * @param {string} gameId
@@ -1051,6 +1077,12 @@ function isMatchPlayed(gameId, matchId) {
     catch(err) {
         console.log("Error from isMatchPlayed: " + err.message);        
     }
+}
+
+
+function isMobileView(){
+    const screenWidth = 768;
+    return $(window).width() < screenWidth 
 }
 
 
